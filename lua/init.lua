519fb9e8ad2a8fe62f4c67f0ae752326106f5c67
Arnold Tharrington <arnoldt@ornl.gov>
2025-01-26 13:09:47 -0500

    Latest working module.

---
+++ lua/init.lua
@@ -0,0 +1,14 @@
+-- 
+print("Loading init module code_comments_abbreviations package.")
+
+local M = {}
+
+function M.config(opts)
+    print("Running config function code_comments_abbreviations package.")
+end
+
+function M.setup(opts)
+    print("Running setup function code_comments_abbreviations package.")
+end
+
+return M
