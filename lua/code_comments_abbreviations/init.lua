26bbe1d0aa73d788dff7f37bf96ebb8daec9dc27
Arnold Tharrington <arnoldt@ornl.gov>
2025-02-09 18:19:52 -0500

    Transient commit.

--- lua/code_comments_abbreviations/init.lua
+++ lua/code_comments_abbreviations/init.lua
@@ -6,7 +6,7 @@ local M = {}
 
 function M.setup(opts)
     print("Running setup function code_comments_abbreviations package.")
-    require("code_comments_abbreviations.python_comments")
+    require("code_comments_abbreviations.python_comments").setup()
 end
 
 return M
