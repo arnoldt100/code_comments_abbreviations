ee4b6c5ecf44264c584e3e5a68de38f347d0036b
Arnold Tharrington <arnoldt@ornl.gov>
2025-02-09 18:28:37 -0500

    Transoent commit.

--- lua/code_comments_abbreviations/init.lua
+++ lua/code_comments_abbreviations/init.lua
@@ -4,6 +4,8 @@ print("Loading init module code_comments_abbreviations package for git repo.")
 
 local M = {}
 
+vim.api.nvim_command('iabbrev pc_2 python_comment 2')
+
 function M.setup(opts)
     print("Running setup function code_comments_abbreviations package.")
     require("code_comments_abbreviations.python_comments").setup()
