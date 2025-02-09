4ac8db9fd26f60661aa1f9f60179698b496cc01d
Arnold Tharrington <arnoldt@ornl.gov>
2025-02-09 17:55:23 -0500

    Transient commit.

--- lua/init.lua
+++ lua/init.lua
@@ -1,13 +1,12 @@
 -- 
 print("Loading init module code_comments_abbreviations package for git repo.")
 
-local pc =  require("code_comments_abbreviations.python_comments")
 
 local M = {}
 
 function M.setup(opts)
     print("Running setup function code_comments_abbreviations package.")
-    pc.setup()
+    require("code_comments_abbreviations.python_comments").setup()
 end
 
 return M
